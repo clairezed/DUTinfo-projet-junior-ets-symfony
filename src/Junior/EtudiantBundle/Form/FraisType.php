2d0f4bdda63c55834ed17b2d08e644eb2126cc90
Claire Zuliani <clairezuliani@gmail.com>
2013-06-08 14:11:05 +0200

    added Frais typeField in Etudiant package

--- src/Junior/EtudiantBundle/Form/FraisType.php
+++ src/Junior/EtudiantBundle/Form/FraisType.php
@@ -6,27 +6,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class FraisType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class FraisType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('typeFrais', 'text')
-            ->add('montantFrais', 'integer')
-            ->add('dateAchat', 'date')
-            ->add('etude')
+                ->add('objetFrais', 'text')
+                ->add('montantFrais')
+                ->add('dateAchat', 'date')
+                ->add('typeFrais', 'choice', array(
+    'choices' => array('Déplacement' => 'Déplacement', 'Séjour' => 'Séjour', 'Autre' => 'Autre')
+))
+                ->add('etude')
         ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Junior\EtudiantBundle\Entity\Frais'
         ));
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'junior_etudiantbundle_fraistype';
     }
+
 }
